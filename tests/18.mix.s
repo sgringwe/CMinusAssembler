d9c8b5e9b706ca5b5fa42afbf197fd45c3b8cd16
Scott Ringwelski <sgringwe@mtu.edu>
2013-10-09 15:42:56 -0400

    write c code to see output for 18

--- tests/18.mix.s
+++ tests/18.mix.s
@@ -1,10 +1,14 @@
-i = 
-j = 
-k = 
-l = 
-m = 
-n = k < j + l && (!i!=k);
-write(n);
-write(i * l > j * k || 1 <= m);
-write(k * l > j + k + l && i !=j || l>n);
-write(l / j - i > m && k + l * n != l + k * n);
+int main() {
+    int i = 1;
+    int j = 2;
+    int k = 3;
+    int l = 4;
+    int m = 5;
+
+    int n = 3 < 2 + 4 && (!1!=3);
+
+    printf("%d\n", n);
+    printf("%d\n", 1 * 4 > 2 * 3 || 1 <= 5);
+    printf("%d\n", 3 * 4 > 2 + 3 + 4 && 1 !=2 || 4>1);
+    printf("%d\n", 4 / 2 - 1 > 5 && 3 + 4 * n != 4 + 3 * 1);
+}
